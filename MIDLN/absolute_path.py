58a08752d0097a821c5e1f22fc26b06e5d7e12c0
fjc1575 <42359195+fjc1575@users.noreply.github.com>
2023-11-05 18:23:59 +0800

    Add files via upload

---
+++ MIDLN/absolute_path.py
@@ -0,0 +1,24 @@
+
+
+import os
+
+
+def listdir(path, list_name):  # 传入存储的list
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        if os.path.isdir(file_path):
+            listdir(file_path, list_name)
+        else:
+            list_name.append(file_path)
+
+
+list_name = []
+path = 'VOCdevkit/images/val'  # 文件夹路径
+listdir(path, list_name)
+print(list_name)
+
+with open('VOCdevkit/val_txt', 'w') as f:  # 要存入的txt
+    write = ''
+    for i in list_name:
+        write = write + str(i) + '\n'
+    f.write(write)
